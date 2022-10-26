21e50bbb5db1fe581fd098541f7fc900cca261d8
frackz <65610875+frackz@users.noreply.github.com>
2022-10-26 20:20:10 +0200

    Delete ready.js

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    event: 'ready', // Event name
-    type: 'on', // Type, 'on' or 'once'
-    load: () => {}, // When this file has been loaded
-    execute: (bot) => {} // When event has been executed
-}
